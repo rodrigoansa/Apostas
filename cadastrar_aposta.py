36d2484fe3bea1724c36856bb7898818c8a6de1d
Rodrigo Ansa <rodrigoansa2@gmail.com>
2022-09-21 12:39:43 -0300

    Update cadastrar_aposta.py

--- cadastrar_aposta.py
+++ cadastrar_aposta.py
@@ -1,12 +1,21 @@
 from PyQt5 import  uic,QtWidgets
 
 def funcao_principal():
-    print('teste')
+    time1 = cadastro.lineEdit.text()
+    time2 = cadastro.lineEdit_2.text()
+    time3 = cadastro.lineEdit_3.text()
+    time4 = cadastro.lineEdit_4.text()
+    time5 = cadastro.lineEdit_5.text()
+    time6 = cadastro.lineEdit_6.text()
+    time7 = cadastro.lineEdit_7.text()
+    time8 = cadastro.lineEdit_8.text()
+    lucro = cadastro.lineEdit_9.text()
+    
     
        
 app=QtWidgets.QApplication([])
-formulario=uic.loadUi("formulario2.ui")
-formulario.pushButton.clicked.connect(funcao_principal)
+cadastro=uic.loadUi("Cadastro.ui")
+cadastro.pushButton.clicked.connect(funcao_principal)
 
-formulario.show()
+cadastro.show()
 app.exec()
