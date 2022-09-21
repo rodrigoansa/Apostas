8cc44ec3ad54e43c3e52f0151cc4422ffd07d800
Rodrigo Ansa <rodrigoansa2@gmail.com>
2022-09-21 19:10:17 -0300

    Update cadastrar_aposta.py

--- cadastrar_aposta.py
+++ cadastrar_aposta.py
@@ -2,22 +2,14 @@ from PyQt5 import  uic,QtWidgets
 
 def funcao_principal():
     time1 = cadastro.lineEdit.text()
-    if time1 != (' '):
-        time2 = cadastro.lineEdit_2.text()
-        if time2 != (' '):
-            time3 = cadastro.lineEdit_3.text()
-            if time3 != (' '):
-                time4 = cadastro.lineEdit_4.text()
-                if time4 != (' '):
-                    time5 = cadastro.lineEdit_5.text()
-                    if time5 != (' '):
-                        time6 = cadastro.lineEdit_6.text()
-                        if time6 != (' '):
-                            time7 = cadastro.lineEdit_7.text()
-                            if time7 != (' '):
-                                time8 = cadastro.lineEdit_8.text()
-                                if time8 != (' '):
-                                    lucro = cadastro.lineEdit_9.text()
+    time2 = cadastro.lineEdit_2.text()    
+    time3 = cadastro.lineEdit_3.text()           
+    time4 = cadastro.lineEdit_4.text()               
+    time5 = cadastro.lineEdit_5.text()               
+    time6 = cadastro.lineEdit_6.text()                       
+    time7 = cadastro.lineEdit_7.text()                          
+    time8 = cadastro.lineEdit_8.text()                            
+    lucro = cadastro.lineEdit_9.text()
         
     if time1 != (' '):
         print('Time 1: ',time1)
