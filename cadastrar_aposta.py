43b14c4df97a44a296d7456e5654a86db7a2b0a4
Rodrigo Ansa <rodrigoansa2@gmail.com>
2022-09-22 11:48:50 -0300

    a

--- cadastrar_aposta.py
+++ cadastrar_aposta.py
@@ -3,55 +3,40 @@ from PyQt5 import  uic,QtWidgets
 def funcao_principal():
     time1 = cadastro.lineEdit.text()
     time2 = cadastro.lineEdit_2.text()
-    if time2 != (' '):    
+    if not time2:    
         time3 = cadastro.lineEdit_3.text()
     else:
-        time4 = cadastro.lineEdit_4.text() 
-    if time3 != (' '):           
-        time4 = cadastro.lineEdit_4.text()  
-    if time4 != (' '):              
+        lucro = cadastro.lineEdit_9.text()
+    if not time3:           
+        time4 = cadastro.lineEdit_4.text()
+    else:
+        lucro = cadastro.lineEdit_9.text()  
+    if not time4:              
         time5 = cadastro.lineEdit_5.text()
-    if time5 != (' '):               
+    else:
+        lucro = cadastro.lineEdit_9.text()
+    if not time5:               
         time6 = cadastro.lineEdit_6.text()
-    if time6 != (' '):                           
-        time7 = cadastro.lineEdit_7.text() 
-    if time7 != (' '):                             
-        time8 = cadastro.lineEdit_8.text()                             
-    lucro = cadastro.lineEdit_9.text()
+    else:
+        lucro = cadastro.lineEdit_9.text()
+    if not time6:                           
+        time7 = cadastro.lineEdit_7.text()
+    else:
+        lucro = cadastro.lineEdit_9.text() 
+    if not time7:                             
+        time8 = cadastro.lineEdit_8.text()
+    else:                             
+        lucro = cadastro.lineEdit_9.text()
         
     print('Time 1: ',time1)
-    
-    if time2 != (' '):
-        print('Time 2: ',time2)
-    else:
-        print('Lucro: ', lucro)
-    if time3 != (' '):
-        print('Time 3: ',time3)
-    else:
-        print('Lucro: ', lucro)
-    if time4 != (' '):
-        print('Time 4: ',time4)
-    else:
-        print('Lucro: ', lucro)
-    if time5 != (' '):
-        print('Time 5: ',time5)
-    else:
-        print('Lucro: ', lucro)
-    if time6 != (' '):
-        print('Time 6: ', time6)
-    else:
-        print('Lucro: ', lucro)
-    if time7 != (' '):
-        print('Time 7: ',time7)
-    else:
-        print('Lucro: ', lucro)
-    if time8 != (' '):
-        print('Time 8: ', time8)
-    else:
-        print('Lucro: ', lucro)
-    print('Lucro: ', lucro)
-
-    
+    print('Time 2: ',time2)
+    print('Time 3: ',time3)
+    print('Time 4: ',time4)
+    print('Time 5: ',time5)
+    print('Time 6: ',time6)
+    print('Time 7: ',time7)
+    print('Time 8: ',time8)
+    print('Lucro: ', lucro) 
     
        
 app=QtWidgets.QApplication([])
