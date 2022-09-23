7c31e40dfae0bb6f33a2534a3c90682e93e4fc0d
Rodrigo Ansa <rodrigoansa2@gmail.com>
2022-09-22 22:28:13 -0300

    Funcionalidade do cadastro

--- cadastrar_aposta.py
+++ cadastrar_aposta.py
@@ -10,25 +10,30 @@ def funcao_principal():
     time7 = cadastro.lineEdit_7.text()
     time8 = cadastro.lineEdit_8.text()
     lucro = cadastro.lineEdit_9.text()
-    radio = cadastro.radioButton.isChecked()
-    
+
+    if cadastro.checkBox_1.isChecked() == True:
+        print('Time 1: ',time1)
+    if cadastro.checkBox_2.isChecked() == True:
+        print('Time 2: ',time2)
+    if cadastro.checkBox_3.isChecked() == True:
+        print('Time 3: ',time3)
+    if cadastro.checkBox_4.isChecked() == True:
+        print('Time 4: ',time4)
+    if cadastro.checkBox_5.isChecked() == True:
+        print('Time 5: ',time5)            
+    if cadastro.checkBox_6.isChecked() == True:
+        print('Time 6: ',time6)
+    if cadastro.checkBox_7.isChecked() == True:
+        print('Time 7: ',time7)
+    if cadastro.checkBox_8.isChecked() == True:
+        print('Time 8: ',time8)
         
-    print('Time 1: ',time1)
-    print('Time 2: ',time2)
-    print('Time 3: ',time3)
-    print('Time 4: ',time4)
-    print('Time 5: ',time5)
-    print('Time 6: ',time6)
-    print('Time 7: ',time7)
-    print('Time 8: ',time8)
     print('Lucro: ', lucro)
-                
-    
        
 app=QtWidgets.QApplication([])
 cadastro=uic.loadUi("Cadastro.ui")
 cadastro.pushButton.clicked.connect(funcao_principal)
-cadastro.radioButton.isChecked()
+
 
 cadastro.show()
 app.exec()
