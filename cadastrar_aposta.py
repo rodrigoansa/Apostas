bb4cce91e8fbd1af0d51638425213b7e9ae00b83
Rodrigo Ansa <rodrigoansa2@gmail.com>
2022-09-23 22:51:53 -0300

    a

--- cadastrar_aposta.py
+++ cadastrar_aposta.py
@@ -11,52 +11,42 @@ def funcao_principal():
     time8 = cadastro.lineEdit_8.text()
     lucro = cadastro.lineEdit_9.text()
 
-    mercado = ['','Casa', 'Empate', 'Fora', '+2,5']
-
-    cadastro.comboBox.addItems(mercado)
-    cadastro.comboBox_1.addItems(mercado)
-    cadastro.comboBox_2.addItems(mercado)
-    cadastro.comboBox_3.addItems(mercado)
-    cadastro.comboBox_4.addItems(mercado)
-    cadastro.comboBox_5.addItems(mercado)
-    cadastro.comboBox_6.addItems(mercado)
-    cadastro.comboBox_7.addItems(mercado)
-
-        
+             
     if cadastro.checkBox_1.isChecked() == True:
-        print('Time 1: ',time1)
-        print(cadastro.comboBox.addItems(mercado))
+        print('Time 1: ',time1, check)
     if cadastro.checkBox_2.isChecked() == True:
-        print('Time 2: ',time2)
-        print(cadastro.comboBox_1.addItems(mercado))
+        print('Time 2: ',time2, check_1)
     if cadastro.checkBox_3.isChecked() == True:
-        print('Time 3: ',time3)
-        print(cadastro.comboBox_2.addItems(mercado))
+        print('Time 3: ',time3, check_2)
     if cadastro.checkBox_4.isChecked() == True:
-        print('Time 4: ',time4)
-        print(cadastro.comboBox_3.addItems(mercado))
+        print('Time 4: ',time4, check_3)
     if cadastro.checkBox_5.isChecked() == True:
-        print('Time 5: ',time5)
-        print(cadastro.comboBox_4.addItems(mercado))            
+        print('Time 5: ',time5, check_4)
     if cadastro.checkBox_6.isChecked() == True:
-        print('Time 6: ',time6)
-        print(cadastro.comboBox_5.addItems(mercado))
+        print('Time 6: ',time6, check_5)
     if cadastro.checkBox_7.isChecked() == True:
-        print('Time 7: ',time7)
-        print(cadastro.comboBox_6.addItems(mercado))
+        print('Time 7: ',time7, check_6)
     if cadastro.checkBox_8.isChecked() == True:
-        print('Time 8: ',time8)
-        print(cadastro.comboBox_7.addItems(mercado))
+        print('Time 8: ',time8, check_7)
         
     print('Lucro: R$', lucro)
+    print(check)
 
+mercado = ['','Casa', 'Empate', 'Fora', '+2,5']
 
-    
       
 app=QtWidgets.QApplication([])
 cadastro=uic.loadUi("Cadastro.ui")
 cadastro.pushButton.clicked.connect(funcao_principal)
 
+check = cadastro.comboBox.addItems(mercado)
+check_1 = cadastro.comboBox_1.addItems(mercado)
+check_2 = cadastro.comboBox_2.addItems(mercado)
+check_3 = cadastro.comboBox_3.addItems(mercado)
+check_4 = cadastro.comboBox_4.addItems(mercado)
+check_5 = cadastro.comboBox_5.addItems(mercado)
+check_6 = cadastro.comboBox_6.addItems(mercado)
+check_7 = cadastro.comboBox_7.addItems(mercado)
 
 
 cadastro.show()
